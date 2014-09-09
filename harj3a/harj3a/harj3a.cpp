86481fdba3045b3442583da7e58443dab3754b9f
ShadeH9281 <ffk7shade@gmail.com>
2014-09-09 14:15:57 +0300

    Harjoitus 3
    Sano jos ei pitäis laittaa kaikkea...

---
+++ harj3a/harj3a/harj3a.cpp
@@ -0,0 +1,6 @@
+#include <iostream> //t‰ss‰ m‰‰ritell‰‰ otsikko tiedosto
+using namespace std;
+void main(void)
+{
+	cout << "Toni Pajukanta\nHirvenpolku 9\n"; /* T‰h‰n kirjoitetaan viesti OO  cout = character out*/
+}
---
+++ harj3b/harj3b/Harj3b.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+void main(void)
+{
+	cout << "**********************************\n* *\n* Toni *\n* Pajukanta *\n* *\n*-------------------------------------- *\n* *\n* Hirvenpolku 9 *\n* 40270 Palokka *\n* *\n**********************************\n";
+}
